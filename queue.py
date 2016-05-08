8110d8b298dc9dcd12cbe8269c2f229ef2603381
Hiroyuki Saito <psyhiro@yahoo.co.jp>
2016-05-08 15:37:20 +0900

    second commit

--- queue.py
+++ queue.py
@@ -1,8 +1,4 @@
-"""Make a Queue class using a list!
-Hint: You can use any Python list method
-you'd like! Try to write each one in as 
-few lines as possible.
-Make sure you pass the test cases too!"""
+#Queue
 
 class Queue:
     def __init__(self, head=None):
