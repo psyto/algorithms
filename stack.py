658ed9450d5dff7fc382943bf4ec9ecda38b648a
Hiroyuki Saito <psyhiro@yahoo.co.jp>
2016-05-08 15:35:24 +0900

    second commit

--- stack.py
+++ stack.py
@@ -1,10 +1,4 @@
-"""Add a couple methods to our LinkedList class,
-and use that to implement a Stack.
-You have 4 functions below to fill in:
-insert_first, delete_first, push, and pop.
-Think about this while you're implementing:
-why is it easier to add an "insert_first"
-function than just use "append"?"""
+#Stack
 
 class Element(object):
     def __init__(self, value):
