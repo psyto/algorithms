a0ce14d433a4a355c405c601d7c9fe8f9f8bb0be
Hiroyuki Saito <psyhiro@yahoo.co.jp>
2016-05-08 15:42:02 +0900

    first commit

---
+++ fibonacci.py
@@ -0,0 +1,12 @@
+#Fibonacci
+
+def get_fib(position):
+    if position == 0 or position == 1:
+        return position
+    else:
+        return get_fib(position - 2) + get_fib(position - 1)
+
+# Test cases
+print get_fib(9)
+print get_fib(11)
+print get_fib(0)
